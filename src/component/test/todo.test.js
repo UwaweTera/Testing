bc9f750326823113e820b25c9eed76e4dec95db6
uwawetera <uwaweteraviv@gmail.com>
2023-03-23 07:35:31 +0200

    changes to comfig yml

--- src/component/test/todo.test.js
+++ src/component/test/todo.test.js
@@ -1,3 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Todo from "../todo"
+import '@testing-library/jest-dom'
+
 test("simple test", ()=>{
     expect(true).toBe(true)
+})
+
+
+test("test for todo element", ()=>{
+    render(<Todo/>);
+    const todoElement = screen.getByTestId('todo-1');
+    expect(todoElement).toBeInTheDocument();
 })
